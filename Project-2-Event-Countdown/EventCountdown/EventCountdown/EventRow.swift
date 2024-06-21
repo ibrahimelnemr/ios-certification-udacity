3e48111eeda1d2fa676d4b7e0ceda0e50f6d9c3b
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2024-06-21 07:53:33 -0700

    event shows relative time since creation

--- Project-2-Event-Countdown/EventCountdown/EventCountdown/EventRow.swift
+++ Project-2-Event-Countdown/EventCountdown/EventCountdown/EventRow.swift
@@ -7,14 +7,31 @@ import SwiftUI
 
 struct EventRow : View {
     let event: Event
+    @State private var currentDate = Date()
+
+    
+    private var relativeDate: String {
+        let formatter = RelativeDateTimeFormatter()
+        formatter.unitsStyle = .full
+        return formatter.localizedString(for: event.date, relativeTo: currentDate)
+    }
     
     var body: some View {
         HStack {
             Text("\t\(event.title)")
                 .foregroundColor(event.textColor)
-            Text("\t\(event.date.formatted())")
+            Spacer()
+            Text("\t\(relativeDate)")
+        }.padding()
+            .onAppear {
+                startTimer()
+            }
+    }
+    
+    private func startTimer() {
+        Timer.scheduledTimer(withTimeInterval: 60, repeats: true) { timer in
+            currentDate = Date()
         }
-        
     }
 }
 
