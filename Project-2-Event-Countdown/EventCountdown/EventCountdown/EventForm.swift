abd58aacb7529f2f2a8aacbdca2b151c49c442ec
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2024-06-21 09:56:52 -0700

    add basic form to eventForm

--- Project-2-Event-Countdown/EventCountdown/EventCountdown/EventForm.swift
+++ Project-2-Event-Countdown/EventCountdown/EventCountdown/EventForm.swift
@@ -7,8 +7,39 @@ import SwiftUI
 
 struct EventForm: View {
     
-    var body: some View {
-        Text("event form")
-    }
+    @State private var title = ""
+    @State private var date = Date()
+    @State private var textColor = Color.black
     
+    var body: some View {
+        NavigationView {
+                 Form {
+                     Section(header: Text("event details")) {
+                         TextField("title", text: $title)
+                     }
+
+                     Section(header: Text("date")) {
+                         DatePicker(selection: $date, in: Date()..., displayedComponents: .date) {
+                             Text("select date")
+                         }
+                     }
+
+                     Section(header: Text("text color")) {
+                         ColorPicker("select text color", selection: $textColor)
+                     }
+                 }
+                 .navigationTitle("Add Event")
+                 .navigationBarItems(trailing: Button("save") {
+                     
+                     // save event
+                     
+                     let newEvent = Event(id: UUID(), title: title, date: date, textColor: textColor)
+                     print("saved event: \(newEvent)")
+                 })
+             }
+    }
+}
+
+#Preview {
+    EventForm()
 }
