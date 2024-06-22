c6846038e23b63bc800bda848f2d88df33fd262e
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2024-06-22 00:06:04 -0700

    make demo dates in future

--- Project-2-Event-Countdown/EventCountdown/EventCountdown/EventsView.swift
+++ Project-2-Event-Countdown/EventCountdown/EventCountdown/EventsView.swift
@@ -7,11 +7,11 @@ import SwiftUI
 
 struct EventsView: View {
     @State var events: [Event] = [
-        Event(id: UUID(), title: "Event 1", date: Date(), textColor: Color.green),
-        Event(id: UUID(), title: "Event 2", date: Date(), textColor: Color.red),
-        Event(id: UUID(), title: "Event 3", date: Date(), textColor: Color.blue)
+        Event(id: UUID(), title: "Event 1", date: Calendar.current.date(byAdding: .minute, value: 5, to: Date())!, textColor: Color.green),
+        Event(id: UUID(), title: "Event 2", date: Calendar.current.date(byAdding: .hour, value: 3, to: Date())!, textColor: Color.red),
+        Event(id: UUID(), title: "Event 3", date: Calendar.current.date(byAdding: .day, value: 2, to: Date())!, textColor: Color.blue)
     ]
-    
+
     var body: some View {
         NavigationStack {
             List {
