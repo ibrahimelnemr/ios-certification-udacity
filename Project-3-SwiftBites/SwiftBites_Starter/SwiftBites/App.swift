fbf762aaf93f750a9c72cbfbb7d519b0f67dbd01
Ibrahim El Nemr <70034333+ibrahimelnemr@users.noreply.github.com>
2024-07-20 00:11:24 -0700

    refresh data for each session

--- Project-3-SwiftBites/SwiftBites_Starter/SwiftBites/App.swift
+++ Project-3-SwiftBites/SwiftBites_Starter/SwiftBites/App.swift
@@ -8,7 +8,8 @@ struct SwiftBitesApp: App {
         WindowGroup {
             ContentView()
 //                .environment(\.storage, Storage())
-                .modelContainer(NewStorageContainer.create())
+                .modelContainer(NewStorageContainer.create(deleteExistingData: true))
+            // Change to deleteExistingData: false for data not to be reset after session
         }
     }
 }
